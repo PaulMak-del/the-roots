9bd5e0fb8b25ee44cc9b4e9116a583c64d6ee982
Павел Хоменко <pawelxom@mail.ru>
2025-06-20 22:13:09 +0300

    Add Angular Material UI-kit

--- src/app/features/welcome/welcome.module.ts
+++ src/app/features/welcome/welcome.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { MatButtonToggle } from '@angular/material/button-toggle';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @NgModule({
     declarations: [WelcomeComponent],
-    imports: [CommonModule, WelcomeRoutingModule],
+    imports: [
+        CommonModule,
+        WelcomeRoutingModule,
+        MatButtonToggle,
+        MatSlideToggle,
+    ],
 })
 export class WelcomeModule {}
